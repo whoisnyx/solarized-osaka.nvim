c1d58c794271016d0d43c36ddbb0fcb53191d8b7
whoisnyx <whoisnux.main@gmail.com>
2025-08-06 11:50:48 +0300

    theme changes

--- lua/solarized-osaka/groups/telescope.lua
+++ lua/solarized-osaka/groups/telescope.lua
@@ -3,9 +3,8 @@ local M = {}
 function M.get(c)
   -- stylua: ignore
   return {
-    TelescopeBorder = { fg = c.base035, bg = c.bg_float },
+    TelescopeBorder = { fg = c.base03, bg = c.bg_float },
     TelescopeNormal = { fg = c.base0, bg = c.bg_float },
-    TelescopeTitle = { fg = c.base045, bg = c.blue500 },
   }
 end
 
